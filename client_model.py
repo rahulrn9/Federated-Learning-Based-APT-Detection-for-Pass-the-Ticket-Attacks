01ed1294e275698c09f54b4b585dcb4ebb138f73
rahulrn9 <149145638+rahulrn9@users.noreply.github.com>
2025-04-06 22:38:19 +0530

    client_model.py

---
+++ client_model.py
@@ -0,0 +1,9 @@
+from models.global_model import GlobalModel
+
+class ClientModel(GlobalModel):
+    """
+    Client model extends GlobalModel. In a real-world use case,
+    this can be extended to include client-specific preprocessing
+    or configurations.
+    """
+    pass
