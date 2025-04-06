ae81996de01d208198fa925eddb180edae654bd9
rahulrn9 <149145638+rahulrn9@users.noreply.github.com>
2025-04-06 22:50:34 +0530

    run_server.sh

---
+++ run_server.sh
@@ -0,0 +1,6 @@
+Shell script to run the federated server :
+#!/bin/bash
+echo "[✓] Starting Federated Server Training..."
+python federated/federated_server.py
+Make it executable:
+chmod +x scripts/run_server.sh
